35fe5af1be6516805ff5bb0d94d49eeb1fda4e4e
Omar Palacios <omarpal88@gmail.com>
2021-01-31 00:10:31 -0500

    Arreglos de errores en archivos

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,8 @@ import Header from '../template/Header';
 import Home from '../pages/Home';
 import Character from '../pages/Character';
 import Error404 from '../pages/Error404';
-import getHash from '..//utils/getHash';
-import resolveRoutes from '..//utils/resolveRoutes';
+import getHash from '../utils/getHash';
+import resolveRoutes from '../utils/resolveRoutes';
 
 const routes = {
   '/': Home,
